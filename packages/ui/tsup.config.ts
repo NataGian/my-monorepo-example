5b0c1485df910d716e237471fc20d1a61cd72577
Natalia Giannouli <n.giannouli@gnomon.com.gr>
2025-05-06 13:15:27 +0300

    try

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,9 +1,13 @@
 import {defineConfig} from 'tsup';
 
 export default defineConfig({
-    entry: ['src/index.ts', 'src/components/**/*.ts', 'src/hooks/**/*.ts', 'src/lib/**/*.ts', 'src/styles/globals.css'],
+    entry: ['src/index.ts'],
+    // 'src/components/**/*.ts', 'src/hooks/**/*.ts', 'src/lib/**/*.ts', 'src/styles/globals.css'],
     format: ['esm', 'cjs'],
-    dts: true,
+    dts: {
+        resolve: true,
+        entry: 'src/index.ts',
+    },
     clean: true,
     outDir: 'dist',
     shims: true,
